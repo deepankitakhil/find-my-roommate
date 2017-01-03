564f83e03f064a471ee66f86683ea8ccdbb31f35
Deepankit <deepankitakhil@gmail.com>
2017-01-03 14:11:10 -0700

    Added VO to store user profile in db

---
+++ findmyroommate/src/main/java/vo/User.java
@@ -0,0 +1,55 @@
+package vo;
+
+import com.google.android.gms.location.places.Place;
+
+/**
+ * Created by akhil on 1/2/2017.
+ */
+
+public class User {
+    private String name;
+    private String phoneNumber;
+    private String profession;
+    private String userBio;
+    private String email;
+    private String sex;
+    private Place address;
+
+    public User(String name, String phoneNumber, String profession, String userBio, String email, String sex, Place address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.profession = profession;
+        this.userBio = userBio;
+        this.email = email;
+        this.sex = sex;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public String getUserBio() {
+        return userBio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Place getAddress() {
+        return address;
+    }
+}
